0663e697f43f0a1842e2b4ba8a4678f3a37eab51
akashyadav7771 <akashyadav88155@gmail.com>
2026-02-26 15:28:02 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,19 +1,4 @@
-// // import jwt from "jsonwebtoken";
 
-// // const generateToken = (res, userId) => {
-// //   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-// //     expiresIn: "7d",
-// //   });
-
-// //   res.cookie("token", token, {
-// //     httpOnly: true,
-// //     secure: process.env.NODE_ENV === "production",
-// //     sameSite: "strict",
-// //     maxAge: 7 * 24 * 60 * 60 * 1000,
-// //   });
-// // };
-
-// // export default generateToken;
 
 import jwt from "jsonwebtoken";
 
@@ -26,17 +11,12 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,        // localhost me false
-    sameSite: "lax",      // 👈 CHANGE THIS
-    path: "/",            // 👈 ADD THIS
+    secure: false,      
+    sameSite: "lax",   
+    path: "/",         
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
 export default generateToken;
 
-// res.cookie("token", token, {
-//   httpOnly: true,
-//   secure: process.env.NODE_ENV === "production",
-//   sameSite: "lax",
-// });
